b5465b93f7a6aca048c37174e052a18e5694993c
nitin patyal <nitinpatyal@nitins-MacBook-Air.local>
2026-01-22 13:10:38 +0530

    add backend

---
+++ src/data/projects.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    title: "Airline Management System",
+    description:
+      "A complete flight booking, payment, and cancellation system with a professional UI and backend integration.",
+    tech: ["React", "Node.js", "SQL"],
+        github: "https://github.com/nitin-patyal-57/AirlineManagementProject",
+    live: "",
+    featured: true,
+  },
+  {
+    title: "AI Career Path Recommender",
+    description:
+      "An AI-powered platform that analyzes user skills and interests to recommend optimal career paths.",
+    tech: ["AI", "Machine Learning", "Web"],
+    featured: true,
+  },
+];
+
+export default projects;
